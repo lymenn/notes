c2a0d474ac60f4d16f36bd722bd76a5ac535c49b
lymenn <lymenn@163.com>
2021-04-27 19:58:34 +0800

    注释

--- Vue/源码/src/core/instance/index.js
+++ Vue/源码/src/core/instance/index.js
@@ -19,9 +19,33 @@ function Vue (options) {
 // Vue.prototype._init
 
 initMixin(Vue)
+
+// 定义：
+// Vue.prototype.$data
+// Vue.prototype.$props
+// Vue.prototype.$set
+// Vue.prototype.$delete
+// Vue.prototype.$watch
 stateMixin(Vue)
+
+// 定义 事件相关的方法:
+// Vue.prototype.on
+// Vue.prototype.once
+// Vue.prototype.off  
+// Vue.prototype.emit
 eventsMixin(Vue)
+
+// 定义
+// Vue.prototype._update
+// Vue.prototype.$forceUpdate
+// Vue.prototype.$destory
 lifecycleMixin(Vue)
+
+// 执行 installRenderHelpers，在 Vue.prototype 对象上安装运行时便利程序
+
+// 定义
+// Vue.prototype.$nextTick
+// Vue.prototype._render
 renderMixin(Vue)
 
 export default Vue
